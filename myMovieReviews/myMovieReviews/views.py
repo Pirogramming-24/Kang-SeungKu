808eec37cfbfd5e221d3774d591f6816f303368b
SeungKu-Kang <yoyuman@gmail.com>
2026-01-01 04:31:34 +0900

    영화 리뷰 사이트 만들기

--- myMovieReviews/myMovieReviews/views.py
+++ myMovieReviews/myMovieReviews/views.py
@@ -12,12 +12,12 @@ def review_list(request):
     reviews = Review.objects.all().order_by('-created_at') # 최신순 정렬
     return render(request, 'myMovieReviews/review_list.html', {'reviews': reviews})
 
-# 2. 리뷰 디테일 페이지 (기능 4, 5, 6, 7)
+
 def review_detail(request, pk):
     review = get_object_or_404(Review, pk=pk)
     return render(request, 'myMovieReviews/review_detail.html', {'review': review})
 
-# 3. 리뷰 작성 페이지 (기능 8, 10)
+
 def review_create(request):
     if request.method == 'POST':
         form = ReviewForm(request.POST)
@@ -28,7 +28,7 @@ def review_create(request):
         form = ReviewForm() # 빈 폼
     return render(request, 'myMovieReviews/review_form.html', {'form': form})
 
-# 4. 리뷰 수정 페이지 (기능 9, 11)
+
 def review_update(request, pk):
     review = get_object_or_404(Review, pk=pk)
     if request.method == 'POST':
@@ -40,9 +40,23 @@ def review_update(request, pk):
         form = ReviewForm(instance=review) # 기존 데이터 채워진 폼
     return render(request, 'myMovieReviews/review_form.html', {'form': form})
 
-# 5. 리뷰 삭제 기능 (기능 7)
+
 def review_delete(request, pk):
     if request.method == 'POST':
         review = get_object_or_404(Review, pk=pk)
         review.delete()
     return redirect('review_list')
+
+def review_detail(request, pk):
+    review = get_object_or_404(Review, pk=pk)
+    
+    
+    hours = review.running_time // 60
+    minutes = review.running_time % 60
+    
+    context = {
+        'review': review,
+        'hours': hours,
+        'minutes': minutes
+    }
+    return render(request, 'myMovieReviews/review_detail.html', context)
