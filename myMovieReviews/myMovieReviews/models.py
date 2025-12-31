fa1fe0f777f5a0f0f03123ca394aa771a692fb29
SeungKu-Kang <170948850+SeungKu-Kang@users.noreply.github.com>
2026-01-01 03:28:26 +0900

    노트북 작업분 커밋

--- myMovieReviews/myMovieReviews/models.py
+++ myMovieReviews/myMovieReviews/models.py
@@ -3,19 +3,30 @@ from django.core.validators import MinValueValidator, MaxValueValidator
 # Create your models here.
 
 class Review(models.Model) :
-    title = models.CharField(max_length=50)
-    release_year = models.IntegerField()
-    genre = models.CharField(max_length=50)
+    
+    GENRE_CHOICES = [
+        ('ACTION', '액션'),
+        ('ROMANCE', '로맨스'),
+        ('COMEDY', '코미디'),
+        ('THRILLER', '스릴러'),
+        ('DRAMA', '드라마'),
+        ('SF', 'SF'),
+    ]
+    
+    title = models.CharField(max_length=200, verbose_name="제목")
+    release_year = models.IntegerField(verbose_name="개봉년도")
+    genre = models.CharField(max_length=100, choices=GENRE_CHOICES, verbose_name="장르")
     rating = models.DecimalField(
         max_digits=4, 
         decimal_places=2,
-        validators=[MinValueValidator(0.0), MaxValueValidator(10.0)] # 0.0 ~ 10.0 제한
+        verbose_name="별점",
+        validators=[MinValueValidator(0.0), MaxValueValidator(10.0)]
     )
 
-    director = models.CharField(max_length=100) 
-    actors = models.CharField(max_length=200)   
-    running_time = models.IntegerField()       
-    content = models.TextField()              
+    running_time = models.IntegerField(verbose_name="러닝타임")
+    content = models.TextField(verbose_name="리뷰 내용")
+    director = models.CharField(max_length=100, verbose_name="감독")
+    actors = models.CharField(max_length=200, verbose_name="주연")           
     # 포스터 이미지 넣기
     poster = models.ImageField(upload_to='posters/', null=True, blank=True) 
     
